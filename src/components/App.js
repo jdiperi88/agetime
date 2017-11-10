01358140ad756e12deb1874928a0a4d0edee4668
Joseph DiPeri <jdiperi88@gmail.com>
2017-11-10 11:32:19 -0500

    added css and form using bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
+import { Form, FormControl, Button} from 'react-bootstrap'
+import './App.css'
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            newDate: ''
+        }
+    }
     render(){
         return ( 
-            <div>Age time component</div>
+            <div className='App'>
+                <Form inline >
+                    <h2> Input your Birthday!</h2>
+                    <FormControl type="date" >
+                    </FormControl>
+                {' '}
+                <Button >
+                    Submit
+                </Button>
+                </Form>
+            </div>
         )
     }
 }
